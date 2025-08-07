10c993e97e6224cee45697ff8d1a987f5a52c239
Safdar Ali Niazi <787alisniazi787@gmail.com>
2025-08-07 15:26:27 +0530

    log fix

--- backend/__tests__/log.test.js
+++ backend/__tests__/log.test.js
@@ -1,5 +1,5 @@
-import request from 'supertest';
-import express from 'express';
+const request = require('supertest');
+const express = require('express');
 
 const app = express();
 app.use(express.json());
